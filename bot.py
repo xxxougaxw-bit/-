6fe2bf610672c0d92108629bc70bbbb07eb9c031
xxxougaxw-bit <xxxougaxw@gmail.com>
2026-02-15 11:47:24 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,11 +94,13 @@ async def lfm(
     interaction: discord.Interaction, 
     mode: Literal["ZW", "FFA", "BOX", "1v1", "通話"], 
     count: Literal[0, 1, 2, 3, 4, 5, 6, 7, 8], # 0を無限として扱います
-    time: Literal["5分後", "10分後", "15分後", "20分後", "30分後", "45分後", "60分後"]
+    time: Literal["今から, 5分後", "10分後", "15分後", "20分後", "30分後", "45分後", "60分後"]
 ):
-    # 表示用の人数設定
-    display_count = "無限" if count == 0 else f"{count}人"
-    """
+# 人数の表示判定
+    if count == 0:
+        display_count = "♾️ "
+    else:
+        display_count = f"あと {count} 名"
     mode: ゲームモード
     count: 募集人数 (最大7人)
     time: 終了時間の目安
@@ -169,6 +171,7 @@ if __name__ == "__main__":
     keep_alive()  # Webサーバーを起動
     token = os.getenv('DISCORD_TOKEN')
     client.run(token)
+
 
 
 
